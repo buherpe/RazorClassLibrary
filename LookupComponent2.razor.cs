4ce5eaad7808de2e3469a9a97d6f170bb87162e1
Anatoly Filyuk <buherpet.38@gmail.com>
2022-10-11 13:22:55 +0500

    добавлен лукап 2

---
+++ LookupComponent2.razor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorClassLibrary
+{
+    public partial class LookupComponent2<TEntity> where TEntity : class, IEntity, new()
+    {
+
+    }
+}
