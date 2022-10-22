38689c2e1af8fcebdb93a75ae49de5d190047859
Anatoly Filyuk <buherpet.38@gmail.com>
2022-10-23 01:43:34 +0500

    .

---
+++ DbContext2.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RazorClassLibrary
+{
+    public class DbContext2 : DbContext
+    {
+        public DbContext2(DbContextOptions options) : base(options)
+        {
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var entries = ChangeTracker.Entries();
+
+            foreach (var entry in entries)
+            {
+                Console.WriteLine($"{entry.Entity} {entry.State}");
+            }
+
+            Console.WriteLine($"{ChangeTracker.DebugView.ShortView}");
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
